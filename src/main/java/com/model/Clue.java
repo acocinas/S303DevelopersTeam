f7b6e89dd3bcaa10bc0ece00e4997698ec43e3e9
acocinas <alcogar79@gmail.com>
2025-05-06 11:39:07 +0200

    refactor: add Lombok @Builder with default value for Clue
    Simplifies Clue creation for Abstract Factory usage and sets revealedClue to false by default using @Builder.Default.

--- src/main/java/com/model/Clue.java
+++ src/main/java/com/model/Clue.java
@@ -5,16 +5,17 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Clue {
     private int idClue;
     private String descriptionClue;
     private String themeClue;
     private double priceClue;
+
+    @Builder.Default
     private boolean revealedClue = false;
 
-
     public void revealClue() {
-
         this.revealedClue = true;
     }
 }
