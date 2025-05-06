02b25a78b7a7a2876387d786e6890ce74fbe3c8b
MaraMarchello <maramarchelo@gmail.com>
2025-05-07 01:05:21 +0200

    Add Entity-Specific DAOs, base classes

---
+++ src/main/java/com/dao/InMemoryDAOFactory.java
@@ -0,0 +1,34 @@
+package com.dao;
+
+import com.interfaces.ClueDAO;
+import com.interfaces.CertificateDAO;
+import com.interfaces.DecorationItemDAO;
+import com.interfaces.PlayerDAO;
+import com.interfaces.RoomDAO;
+import com.dao.TicketSaleDAO;
+import com.dao.ClueDAOImpl;
+import com.dao.CertificateDAOImpl;
+import com.dao.DecorationItemDAOImpl;
+import com.dao.PlayerDAOImpl;
+import com.dao.RoomDAOImpl;
+import com.dao.TicketSaleDAOImpl;
+import com.interfaces.DAOFactory;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class InMemoryDAOFactory implements DAOFactory {
+    private static final InMemoryDAOFactory INSTANCE = new InMemoryDAOFactory();
+    
+    @Getter private final RoomDAO roomDAO = new RoomDAOImpl();
+    @Getter private final ClueDAO clueDAO = new ClueDAOImpl();
+    @Getter private final DecorationItemDAO decorationItemDAO = new DecorationItemDAOImpl();
+    @Getter private final PlayerDAO playerDAO = new PlayerDAOImpl();
+    @Getter private final CertificateDAO certificateDAO = new CertificateDAOImpl();
+    @Getter private final TicketSaleDAO ticketSaleDAO = new TicketSaleDAOImpl();
+    
+    public static InMemoryDAOFactory getInstance() {
+        return INSTANCE;
+    }
+}
---
+++ src/main/java/com/dao/RoomDAOImpl.java
@@ -0,0 +1,93 @@
+package com.dao;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.interfaces.RoomDAO;
+import com.dao.exception.DAOException;
+import com.enums.Difficulty;
+import com.model.Room;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RoomDAOImpl extends BaseDAO<Room, Integer> implements RoomDAO {
+    
+    @Override
+    public Room create(Room entity) throws DAOException {
+        Integer id = idSequence.getAndIncrement();
+        setEntityId(entity, id);
+        entities.put(id, entity);
+        log.info("Created room with id: {}", id);
+        return entity;
+    }
+
+    @Override
+    public Room update(Room entity) throws DAOException {
+        Integer id = getEntityId(entity);
+        if (id != null && entities.containsKey(id)) {
+            entities.put(id, entity);
+            log.info("Updated room with id: {}", id);
+            return entity;
+        }
+        log.error("Room not found for update: {}", id);
+        throw new DAOException("Room not found for update: " + id);
+    }
+
+    @Override
+    public List<Room> findByDifficulty(Difficulty difficulty) throws DAOException {
+        log.debug("Finding rooms by difficulty: {}", difficulty);
+        return entities.values().stream()
+                .filter(room -> room.getDifficulty().equals(difficulty))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Room> findByTheme(String theme) throws DAOException {
+        log.debug("Finding rooms by theme: {}", theme);
+        return entities.values().stream()
+                .filter(room -> theme.equalsIgnoreCase(room.getTheme()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Room> findRoomsWithPriceRange(double minPrice, double maxPrice) throws DAOException {
+        log.debug("Finding rooms with price range: {} - {}", minPrice, maxPrice);
+        return entities.values().stream()
+                .filter(room -> room.getPrice() >= minPrice && room.getPrice() <= maxPrice)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public double calculateTotalRoomValue() throws DAOException {
+        double total = entities.values().stream()
+                .mapToDouble(Room::getPrice)
+                .sum();
+        log.debug("Calculated total room value: {}", total);
+        return total;
+    }
+
+    @Override
+    public void addClueToRoom(Integer roomId, Integer clueId) throws DAOException {
+        log.info("Adding clue {} to room {}", clueId, roomId);
+        // Implementation would depend on how Rooms and Clues are linked
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    public void addDecorationToRoom(Integer roomId, Integer decorationId) throws DAOException {
+        log.info("Adding decoration {} to room {}", decorationId, roomId);
+        // Implementation would depend on how Rooms and Decorations are linked
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Override
+    protected Integer getEntityId(Room entity) {
+        return entity.getId();
+    }
+
+    @Override
+    protected void setEntityId(Room entity, Integer id) {
+        entity.setId(id);
+    }
+}
