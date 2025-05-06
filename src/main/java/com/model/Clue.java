c04b2c676e7d98f3527c27285b6b368feee71143
acocinas <alcogar79@gmail.com>
2025-05-06 11:10:59 +0200

    refactor: simplify Clue class using Lombok annotations
    Replaced manual getters, setters, constructors, and toString with Lombok's @Data, @AllArgsConstructor, and @NoArgsConstructor. Improves code readability and reduces boilerplate.

--- src/main/java/com/model/Clue.java
+++ src/main/java/com/model/Clue.java
@@ -1,41 +1,20 @@
 package com.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Setter
-@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Clue {
     private int idClue;
     private String descriptionClue;
     private String themeClue;
     private double priceClue;
-    private boolean revealedClue;
+    private boolean revealedClue = false;
 
-    public Clue(int idClue, String descriptionClue, String themeClue, double priceClue) {
-        this.idClue = idClue;
-        this.descriptionClue = descriptionClue;
-        this.themeClue = themeClue;
-        this.priceClue = priceClue;
-        this.revealedClue = false;
-    }
-
-    public Clue() {
-        this.revealedClue = false;
-    }
 
     public void revealClue() {
-        this.revealedClue = true;
-    }
 
-    @Override
-    public String toString() {
-        return "Clue{" +
-                "idClue=" + idClue +
-                ", descriptionClue='" + descriptionClue + '\'' +
-                ", themeClue='" + themeClue + '\'' +
-                ", priceClue=" + priceClue +
-                ", revealedClue=" + revealedClue +
-                '}';
+        this.revealedClue = true;
     }
 }
