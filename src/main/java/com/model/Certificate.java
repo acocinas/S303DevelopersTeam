edf95e4ce005fc2942a53537eb40a0129c520b95
MaraMarchello <maramarchelo@gmail.com>
2025-05-07 22:13:03 +0200

    Fix issues of some methods in Repository pattern

--- src/main/java/com/model/Certificate.java
+++ src/main/java/com/model/Certificate.java
@@ -2,9 +2,13 @@ package com.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Builder
+@AllArgsConstructor
 @Setter
 @Getter
 public class Certificate{
