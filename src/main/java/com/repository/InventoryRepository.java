c79031c04372d154324dcef648499750156bb93d
MaraMarchello <maramarchelo@gmail.com>
2025-05-07 12:49:28 +0200

    Add repository pattern classes for database

---
+++ src/main/java/com/repository/InventoryRepository.java
@@ -0,0 +1,72 @@
+package com.repository;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.interfaces.ClueDAO;
+import com.interfaces.DecorationItemDAO;
+import com.interfaces.RoomDAO;
+import com.dao.exception.DAOException;
+import com.dao.DAOManager;
+import com.enums.Difficulty;
+import com.enums.Material;
+import com.model.Clue;
+import com.model.DecorationItem;
+import com.model.Room;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class InventoryRepository {
+    private final RoomDAO roomDAO;
+    private final ClueDAO clueDAO;
+    private final DecorationItemDAO decorationItemDAO;
+    
+    public InventoryRepository() {
+        roomDAO = DAOManager.getDAOFactory().getRoomDAO();
+        clueDAO = DAOManager.getDAOFactory().getClueDAO();
+        decorationItemDAO = DAOManager.getDAOFactory().getDecorationItemDAO();
+    }
+    
+    public double calculateTotalInventoryValue() throws DAOException {
+        double roomValue = roomDAO.calculateTotalRoomValue();
+        double decorationValue = decorationItemDAO.calculateTotalDecorationValue();
+        
+        double total = roomValue + decorationValue;
+        log.info("Total inventory value calculated: {}", total);
+        return total;
+    }
+    
+    public Map<Difficulty, Long> getRoomCountByDifficulty() throws DAOException {
+        log.debug("Getting room count by difficulty");
+        return roomDAO.findAll().stream()
+                .collect(Collectors.groupingBy(Room::getDifficulty, Collectors.counting()));
+    }
+    
+    public Map<Material, Long> getDecorationCountByMaterial() throws DAOException {
+        log.debug("Getting decoration count by material");
+        return decorationItemDAO.findAll().stream()
+                .collect(Collectors.groupingBy(DecorationItem::getMaterial, Collectors.counting()));
+    }
+    
+    public Map<String, Long> getClueCountByTheme() throws DAOException {
+        log.debug("Getting clue count by theme");
+        return clueDAO.findAll().stream()
+                .collect(Collectors.groupingBy(Clue::getTheme, Collectors.counting()));
+    }
+    
+    public void removeRoomFromInventory(Integer roomId) throws DAOException {
+        log.info("Removing room from inventory: {}", roomId);
+        roomDAO.deleteById(roomId);
+    }
+    
+    public void removeClueFromInventory(Integer clueId) throws DAOException {
+        log.info("Removing clue from inventory: {}", clueId);
+        clueDAO.deleteById(clueId);
+    }
+    
+    public void removeDecorationFromInventory(Integer decorationId) throws DAOException {
+        log.info("Removing decoration from inventory: {}", decorationId);
+        decorationItemDAO.deleteById(decorationId);
+    }
+}
