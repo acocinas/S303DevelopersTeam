51b81ded1657c32b248592af7fd8e6da0a56d8a8
MaraMarchello <maramarchelo@gmail.com>
2025-05-12 16:44:41 +0200

    Fix similar log messages issue

--- src/main/java/com/dao/PlayerDAOImpl.java
+++ src/main/java/com/dao/PlayerDAOImpl.java
@@ -2,7 +2,6 @@ package com.dao;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.interfaces.PlayerDAO;
 import com.dao.exception.DAOException;
